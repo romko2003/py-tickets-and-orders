86fee432e0c6549597227d2be8c1cd033676af96
= <=>
2025-06-16 13:42:44 +0300

    implement py-

--- db/models.py
+++ db/models.py
@@ -78,6 +78,7 @@ class Actor(models.Model):
     def __str__(self) -> str:
         return f"{self.first_name} {self.last_name}"
 
+
 class Genre(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
