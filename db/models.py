7f44bb778d2c7409f30c74c8ab7f3270eac2bae7
= <=>
2025-06-16 13:42:37 +0300

    implement py-

--- db/models.py
+++ db/models.py
@@ -77,3 +77,9 @@ class Actor(models.Model):
 
     def __str__(self) -> str:
         return f"{self.first_name} {self.last_name}"
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
